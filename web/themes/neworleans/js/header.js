1c2992d8cbc05c15d763fc673acfc9fed28d48d3
Hugo-Roy <roy.hugo21@gmail.com>
2021-06-19 15:28:56 +0200

    add libraries, made css and js work

---
+++ web/themes/neworleans/js/header.js
@@ -0,0 +1,8 @@
+const header = document.querySelector('.layout-container');
+function changeBGColor() {
+  header.style.backgroundColor="blue";
+}
+
+// header.addEventListener('click', function(e) {
+//   changeBGColor();
+// });
